f96863eb0ae3d9a4f5ba7526cafa93cca1acec26
Vaibhav9853 <vaibhav09853@gmail.com>
2026-08-28 10:24:05 +0530

    Time: 6 ms (97.23%) | Memory: 8.7 MB (98.33%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    int solve(string &s, string &t, int i, int j, vector<vector<int>> &dp){
-        if(j == t.size()) return 1;
-        if(i >= s.size()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
+    // int solve(string &s, string &t, int i, int j, vector<vector<int>> &dp){
+    //     if(j == t.size()) return 1;
+    //     if(i >= s.size()) return 0;
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-        int not_take = solve(s,t,i+1,j,dp);
-        int take = 0;
-        if(t[j] == s[i]) take = solve(s,t,i+1,j+1,dp);
+    //     int not_take = solve(s,t,i+1,j,dp);
+    //     int take = 0;
+    //     if(t[j] == s[i]) take = solve(s,t,i+1,j+1,dp);
 
-        return dp[i][j] = take + not_take;
-    }
+    //     return dp[i][j] = take + not_take;
+    // }
     int numDistinct(string s, string t) {
         int m = s.size();
         int n = t.size();
 
-        vector<vector<unsigned int>> dp(m+1, vector<unsigned int> (n+1,0));
-        for(int i=0;i<=m;i++) dp[i][0] = 1;
+        vector<unsigned int> dp(n+1,0);
+        dp[0] = 1;
 
         for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                unsigned int not_take = dp[i-1][j];
+            for(int j=min(i,n);j>=1;j--){
+                unsigned int not_take = dp[j];
                 unsigned int take = 0;
-                if(s[i-1] == t[j-1]) take = dp[i-1][j-1];
+                if(s[i-1] == t[j-1]) take = dp[j-1];
 
-                dp[i][j] = take + not_take; 
+                dp[j] = take + not_take; 
             }
         }
-        return dp[m][n];
+        return dp[n];
     }
 };
