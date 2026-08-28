b26fb7fb560c57aaf464a45dea6b6363dfaacd82
Vaibhav9853 <vaibhav09853@gmail.com>
2026-08-28 10:03:58 +0530

    Time: 19 ms (79.74%) | Memory: 27 MB (53.07%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -15,7 +15,18 @@ public:
         int m = s.size();
         int n = t.size();
 
-        vector<vector<int>> dp(m, vector<int> (n,-1));
-        return solve(s,t,0,0,dp);
+        vector<vector<unsigned int>> dp(m+1, vector<unsigned int> (n+1,0));
+        for(int i=0;i<=m;i++) dp[i][0] = 1;
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                unsigned int not_take = dp[i-1][j];
+                unsigned int take = 0;
+                if(s[i-1] == t[j-1]) take = dp[i-1][j-1];
+
+                dp[i][j] = take + not_take; 
+            }
+        }
+        return dp[m][n];
     }
 };
