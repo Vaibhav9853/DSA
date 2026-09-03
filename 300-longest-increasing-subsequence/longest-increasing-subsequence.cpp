b2aafa0f2997169a8c8973d0adc0ba954d0bd769
Vaibhav9853 <vaibhav09853@gmail.com>
2026-09-03 15:39:14 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (94.77%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    // int solve(int i, int l_i, vector<int> &nums, vector<vector<int>> &dp){
-    //     if(i >= nums.size()) return 0;
-    //     if(dp[i][l_i+1] != -1) return dp[i][l_i+1];
-
-    //     int not_take = solve(i+1, l_i, nums, dp);
-    //     int take = INT_MIN;
-    //     if(l_i == -1 || nums[i] > nums[l_i]){
-    //         take = 1+solve(i+1, i, nums, dp);
-    //     }
-
-    //     return dp[i][l_i+1] = max(take, not_take);
-    // }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, 1);
-        int ans = 1;
+        vector<int> temp;
 
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j] < nums[i]){
-                    dp[i] = max(dp[i], dp[j]+1);
-                }
+        for(int i=0;i<n;i++){
+            if(temp.size() > 0 && nums[i] <= temp.back()) {
+                int idx = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+                temp[idx] = nums[i];
             }
-            ans = max(ans, dp[i]);
+            else{
+                temp.push_back(nums[i]);
+            }
         }
-        return ans;;
+        return temp.size();
     }
 };
