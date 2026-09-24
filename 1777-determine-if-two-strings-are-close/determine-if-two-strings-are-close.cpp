ca3270876a89b37f509760e8ce9c5389948acdb9
Vaibhav9853 <vaibhav09853@gmail.com>
2026-09-24 12:22:35 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (49.11%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        if(word1.size() != word2.size()) return false;
+        if(word1.size() != word2.size())
+            return false;
 
-        int n = word1.size();
-        vector<int> freq(26,0);
-        vector<int> freq2(26,0);
-        unordered_map<int, int> mp;
+        vector<int> a(26, 0), b(26, 0);
 
-        for(char ch:word1){
-            freq[ch-'a']++;
-        }
-        for(int i=0;i<26;i++){
-            mp[freq[i]]++;
-        }
-        for(char ch:word2){
-            freq2[ch-'a']++;
-        }
-        for(int i=0;i<26;i++){
-            mp[freq2[i]]--;
-        }
+        for(char c : word1)
+            a[c - 'a']++;
 
-        for(auto it:mp){
-            if(it.second != 0){
+        for(char c : word2)
+            b[c - 'a']++;
+
+        // Same set of characters
+        for(int i = 0; i < 26; i++){
+            if((a[i] == 0) != (b[i] == 0))
                 return false;
-            }
         }
 
-        for(int i=0;i<26;i++){
-            if(freq[i] > 0){
-                if(freq2[i] == 0) return false;
-            }
-            else if(freq2[i] > 0){
-                if(freq[i] == 0) return false;
-            }
-        }
+        // Same multiset of frequencies
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
 
-        return true;
+        return a == b;
     }
 };
